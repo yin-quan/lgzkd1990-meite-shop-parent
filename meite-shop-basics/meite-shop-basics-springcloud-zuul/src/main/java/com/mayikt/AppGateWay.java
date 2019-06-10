41000625b5534ef6c0dcfa6c39572132e2d480ba
lgzkd1990 <wodeshijie1234@126.com>
2019-06-10 21:55:26 +0800

    apollo

--- meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/AppGateWay.java
+++ meite-shop-basics/meite-shop-basics-springcloud-zuul/src/main/java/com/mayikt/AppGateWay.java
@@ -1,5 +1,12 @@
 package com.mayikt;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +24,12 @@ import java.util.List;
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableSwagger2Doc
+@EnableApolloConfig
 public class AppGateWay {
+    // 获取ApolloConfig
+    @ApolloConfig
+    private Config appConfig;
+
     public static void main(String[] args) {
         SpringApplication.run(AppGateWay.class, args);
     }
@@ -26,14 +38,49 @@ public class AppGateWay {
     @Component
     @Primary
     class DocumentationConfig implements SwaggerResourcesProvider {
+        // 访问swagger-ui页面的时，每次都会访问。
+        @SuppressWarnings("rawtypes")
         @Override
         public List<SwaggerResource> get() {
+            // 开启监听，配置文件发生改变需要更改
+/*            appConfig.addChangeListener(new ConfigChangeListener() {
+                @Override
+                public void onChange(ConfigChangeEvent changeEvent) {
+                    get();
+                }
+            });*/
+            return resources();
+        }
+
+        /**
+         * 从阿波罗服务器中获取resources
+         *
+         * @return
+         */
+        private List<SwaggerResource> resources() {
             List resources = new ArrayList<>();
             // app-itmayiedu-order
-            //使用的是服务的别名获取远程SwaggerApi
-            resources.add(swaggerResource("app-mayikt-member", "/app-mayikt-member/v2/api-docs", "2.0"));
-            resources.add(swaggerResource("app-mayikt-weixin", "/app-mayikt-weixin/v2/api-docs", "2.0"));
+            // 网关使用服务别名获取远程服务的SwaggerApi
+            String swaggerDocJson = swaggerDocument();
+            JSONArray jsonArray = JSONArray.parseArray(swaggerDocJson);
+            for (Object object : jsonArray) {
+                JSONObject jsonObject = (JSONObject) object;
+                String name = jsonObject.getString("name");
+                String location = jsonObject.getString("location");
+                String version = jsonObject.getString("version");
+                resources.add(swaggerResource(name, location, version));
+            }
             return resources;
+        }
+
+        /**
+         * 获取swaggerDocument配置
+         *
+         * @return
+         */
+        private String swaggerDocument() {
+            String property = appConfig.getProperty("mayikt.zuul.swagger.document", "");
+            return property;
         }
 
         private SwaggerResource swaggerResource(String name, String location, String version) {
--- meite-shop-service-impl/meite-shop-service-weixin/src/main/java/com/mayikt/service/impl/WeiXinAppServiceImpl.java
+++ meite-shop-service-impl/meite-shop-service-weixin/src/main/java/com/mayikt/service/impl/WeiXinAppServiceImpl.java
@@ -2,14 +2,16 @@ package  com.mayikt.service.impl;
 
 import com.mayikt.AppEntity;
 import com.mayikt.WeiXinAppService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeiXinAppServiceImpl implements WeiXinAppService {
 
-    //@GetMapping("/getApp")
+    @Value("${mayikt.weixin.name}")
+    private String name;
     public AppEntity getApp() {
-        return new AppEntity("644064779", "yushengjun644");
+        return new AppEntity("644064779", name);
     }
 
 }
